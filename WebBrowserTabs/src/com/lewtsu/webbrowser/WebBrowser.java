fd5a31705629da440dc60ecba4172fd636cffe18
s5050337 <lewlaao@outlook.com>
2014-11-06 17:11:37 +0700

    webbrowsertab
    เว็บ

---
+++ WebBrowserTabs/src/com/lewtsu/webbrowser/WebBrowser.java
@@ -0,0 +1,12 @@
+package com.lewtsu.webbrowser;
+
+public class WebBrowser {
+
+	public static void main(String[] args) {
+
+		GuiTabs guiTabs = new GuiTabs();
+		guiTabs.setVisable(true);
+
+	}
+
+}
